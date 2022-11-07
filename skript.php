31393d1b231255d4f2fe7781be41c6df272d7ed4
j-eppe <jesper.blom68@gmail.com>
2022-11-07 10:53:44 +0100

    first commit

---
+++ skript.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webbserverprogrammering";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if (
+            $row["username"] == $_POST["username"] &&
+            $row["password"] == $_POST["password"]
+        ) {
+            $login_success = true;
+            echo "Hejsan ", $_POST["username"];
+        } else {
+            echo "Användare existerar inte";
+        }
+    }
+} else {
+    echo "Databas inkomplett";
+}
+$conn->close();
